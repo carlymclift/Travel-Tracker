0d8e0f7681fafbb4bf19f2aecd73f80d0e82bc7a
Carly Clift <carlymclift@gmail.com>
2020-06-05 11:50:52 -0600

    Write tests for logIn method so that users and agents can use it

--- test/User-test.js
+++ test/User-test.js
@@ -13,7 +13,28 @@ describe('User', () => {
     expect(User).to.be.a('function');
   })
 
-  it('should be an instance of a User', () => {
+  it('Should be an instance of a User', () => {
     expect(user).to.be.an.instanceOf(User);
+  })
+
+  it('Should have a id and a name for the user', () => {
+    expect(user.id).to.eql(7);
+    expect(user.name).to.eql('Emmet Sandham');
+  })
+
+  it('Should hold a password', () => {
+    expect(user.password).to.eql('travel2020');
+  })
+
+  it('Should be able to log into Travel Tracker as a user', () => {
+    expect(user.logIn('traveler7', 'travel2020')).to.equal('Hello Emmet Sandham!');
+  })
+
+  it('Should be able to log into Travel Tracker as a travel agent', () => {
+    expect(user.logIn('agency', 'travel2020')).to.equal('Welcome Travel Agent!');
+  })
+
+  it('Should tell the user if they have the wrong login info', () => {
+    expect(user.logIn('nancy134', 'travel2020')).to.equal('The username or password is inncorrect')
   })
 });
