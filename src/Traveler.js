2088c1e69daf7862d3c1295200e921ffacc839e7
Carly Clift <carlymclift@gmail.com>
2020-06-07 17:21:15 -0600

    Finish passing tests for the user calculating cost of trips

--- src/Traveler.js
+++ src/Traveler.js
@@ -1,18 +1,26 @@
 import User from './User.js';
+import trips from '../data/trips-test-data.js';
+import destinations from '../data/destinations-test-data.js';
 
 class Traveler extends User {
   constructor(userData) {
     super(userData);
-    this.trips = [];
+    this.type = userData.travelerType;
+    this.userTrips = trips.filter(trip => trip.userID === this.id);
     this.moneySpent = 0;
   }
 
-  addVactionSpot() {
-    this.trips = this.userData.trips.filter(location => location.id)
-    console.log(location.id)
+  calculateAmountSpentThisYear(year) {
+    let thisYearTrips = this.userTrips.filter(trip => trip.date.includes(year) && trip.status === 'approved')
+    return destinations.reduce((acc, destination) => {
+      thisYearTrips.forEach(yearTrip => {
+        if (yearTrip.destinationID === destination.id) {
+          acc += (destination.estimatedLodgingCostPerDay * yearTrip.duration) + (destination.estimatedFlightCostPerPerson * yearTrip.travelers)
+        }
+      })
+      return Math.round(acc + (acc * .10));
+    }, 0);
   }
-
-
 }
 
 export default Traveler;
